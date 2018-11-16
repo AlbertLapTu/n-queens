b9b83a1e531c1b43899b5d04698cebe8df3c684a
danieljhegeman <student@hackreactor.com>
2018-11-15 19:20:58 -0800

    Completed findNQueensSolution and countNQueensSolutions.

--- src/solvers.js
+++ src/solvers.js
@@ -17,120 +17,103 @@
   /*
     - Given a value n, when placed on that row, all values after that n index will not be counted
       - All values in the given column[index] below the n value will also not be counted
-    
   */
-  // iterate through board
-  // toggle piece 
-    // call row helper
-    // call col helper
-  // 
-  // var len = n;
-  // var resultsArray = [];
-  // for (var row = 0; row < len; row++) {
-  //   for (var col = 0; col < len; col++) {
-  //     var board = new Board({'n': n});
-  //     var count = 0;
-  //     board.togglePiece(row, col);
-  //     count++;
-  //     if (n === 1) {
-  //       resultsArray.push(board.rows());
-  //     }
-  //     for (var newRow = row + 1; newRow < len; newRow++) {
-  //       for (var newCol = 0; newCol < len; newCol++) {
-  //         if (newRow !== row && newCol !== col) {
-  //           board.togglePiece(newRow,newCol);
-  //         }
-  //         if (board.hasRowConflictAt(newRow)) {
-  //           board.togglePiece(newRow, newCol);
-  //         } else if (board.hasColConflictAt(newCol)) {
-  //           board.togglePiece(newRow, newCol);
-  //         }
-  //         if (board.rows()[newRow][newCol] === 1) {
-  //           count++;
-  //           if (count === n) {
-  //             resultsArray.push(board.rows());
+  
+  // var placePiece = function(currentBoard, row, col) {
+  //   if (counter !== n) {
+  //     for (var newRow = row; newRow < n; newRow++) {
+  //       for (var newCol = 0; newCol < n; newCol++) {
+  //         currentBoard.togglePiece(newRow, newCol);
+  //         if (currentBoard.hasRowConflictAt(newRow) || currentBoard.hasColConflictAt(newCol)) {
+  //           currentBoard.togglePiece(newRow, newCol);
+  //         } else {
+  //           counter++;
+  //           if (counter === n) {
+  //             var answer = _.map(currentBoard.rows(), (el) => el.slice());
+  //             resultsArr.push(answer);
+  //             currentBoard.togglePiece(newRow, newCol);
+  //             counter--;
   //             break;
   //           }
-  //           break;
-  //         }
-  //         if (count === n) {
-  //           resultsArray.push(board.rows());
-  //           break;
+  //           placePiece(currentBoard, newRow + 1, newCol);
+  //           currentBoard.togglePiece(newRow, newCol);
+  //           counter--;
   //         }
   //       }
-  //       if (count === n) {
+  //       if (counter === n) {
   //         break;
   //       }
   //     }
   //   }
   // }
-  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(board.rows()));
+  
+  // placePiece(board, row, col);
+  // return resultsArr[0];
 
-// window.findNRooksSolution = function(n) {
-//   var resultsArr = [];
-//   var counter = 0;
-//   var board = new Board({'n': n});
-//   var row = 0;
-//   var col = 0;
+window.findNRooksSolution = function(n) {
+  var solution;
+  var board = new Board({'n': n});
+  var counter = 0;
   
-//       //Iterate through rows set equal to row
-//       //Iterate through columns set equal to col
-//         //togglePiece()
-//           //Check if any rook conflicts(row/column)
-//             //If there's a conflict, toggle back to 0;
-//             // If there isnt any conflict
-//               //Feed current board back into placePiece
-//               // increment counter
+  var toggler = function(row) {
+    for (var col = 0; col < n; col++) {
+      board.togglePiece(row, col);
+      counter++;
+      if (board.hasAnyRooksConflicts()) {
+        board.togglePiece(row, col);
+        counter--; 
+      } else if (counter === n) {
+        solution = _.map(board.rows(), el => el.slice());
+      } else {
+        toggler(row + 1);
+        board.togglePiece(row, col);
+        counter--;
+      }
+    }
+  }
   
-//   var placePiece = function(currentBoard, row, col) {
-//     if (counter !== n) {
-//       for (var newRow = row; newRow < n; newRow++) {
-//         for (var newCol = 0; newCol < n; newCol++) {
-//           currentBoard.togglePiece(newRow, newCol);
-//           if (currentBoard.hasRowConflictAt(newRow) || currentBoard.hasColConflictAt(newCol)) {
-//             currentBoard.togglePiece(newRow, newCol);
-//           } else {
-//             counter++;
-//             if (counter === n) {
-//               var answer = _.map(currentBoard.rows(), (el) => el.slice());
-//               resultsArr.push(answer);
-//               currentBoard.togglePiece(newRow, newCol);
-//               counter--;
-//               break;
-//             }
-//             placePiece(currentBoard, newRow + 1, newCol);
-//             currentBoard.togglePiece(newRow, newCol);
-//             counter--;
-//           }
-//         }
-//         if (counter === n) {
-//           break;
-//         }
-//       }
-//     }
-//   }
+  toggler(0);
   
-//   placePiece(board, row, col);
-//   return resultsArr[0];
-// };
+  return solution;
+};
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-// window.countNRooksSolutions = function(n) {
-//   var solutionsArray = findNRooksSolution(n);
-//   console.log(solutionsArray);
-//   var solutionCount = solutionsArray.length; //fixme
-
-//   // console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-//   return solutionCount;
-// };
+window.countNRooksSolutions = function(n) {
+  var solutionCount = 0; //fixme
+  var board = new Board({'n': n});
+  var counter = 0;
+  
+  var toggler = function(row) {
+    for (var col = 0; col < n; col++) {
+      board.togglePiece(row, col);
+      counter++;
+      if (board.hasAnyRooksConflicts()) {
+        board.togglePiece(row, col);
+        counter--; 
+      } else if (counter === n) {
+        solutionCount++;
+        board.togglePiece(row, col);
+        counter--;
+      } else {
+        toggler(row + 1);
+        board.togglePiece(row, col);
+        counter--;
+      }
+    }
+  }
+  
+  toggler(0);
+  
+  return solutionCount;
+};
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
   var solution;
   var board = new Board({'n': n});
   var counter = 0;
-  if (n === 0) {
-    return [];
+  if (n === 0 || n === 2 || n === 3) {
+    return _.map(board.rows(), el => el.slice());
   }
   var toggler = function(row) {
     for (var col = 0; col < n; col++) {
@@ -141,7 +124,6 @@ window.findNQueensSolution = function(n) {
         counter--; 
       } else if (counter === n) {
         solution = _.map(board.rows(), el => el.slice());
-        console.log(solution);
       } else {
         toggler(row + 1);
         board.togglePiece(row, col);
@@ -149,7 +131,6 @@ window.findNQueensSolution = function(n) {
       }
     }
   }
-
   toggler(0);
   // console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
@@ -157,8 +138,35 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
-
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
+  var solutionCount = 0; //fixme
+  var board = new Board({'n': n});
+  var counter = 0;
+  if (n === 0) {
+    return 1;
+  } else if (n === 2 || n === 3) {
+    return 0;
+  }
+  
+  var toggler = function(row) {
+    for (var col = 0; col < n; col++) {
+      board.togglePiece(row, col);
+      counter++;
+      if (board.hasAnyQueensConflicts()) {
+        board.togglePiece(row, col);
+        counter--; 
+      } else if (counter === n) {
+        solutionCount++;
+        board.togglePiece(row, col);
+        counter--;
+      } else {
+        toggler(row + 1);
+        board.togglePiece(row, col);
+        counter--;
+      }
+    }
+  }
+  
+  toggler(0);
+  
   return solutionCount;
 };
